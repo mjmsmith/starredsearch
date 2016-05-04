4ac26d7329c704924f3cfd8e397bf727c94ee1bb
Mark Smith <mark@camazotz.com>
2016-05-04 12:45:05 -0400

    Avoid trailing ? if no query items.

--- App/NSURLComponents.swift
+++ App/NSURLComponents.swift
@@ -4,13 +4,12 @@ extension NSURLComponents {
   // TODO: Why can't I declare a failabile initializer version of this?
   
   static func componentsWith(string string: String, queryDict: [String:String]) -> NSURLComponents? {
-    guard let components = NSURLComponents(string: string)
-    else {
-      return nil
-    }
+    guard let components = NSURLComponents(string: string) else { return nil }
 
-    components.queryItems = queryDict.map{ (key, value) in
-      return NSURLQueryItem(name: key, value: value)
+    if queryDict.count > 0 {
+      components.queryItems = queryDict.map { key, value in
+        return NSURLQueryItem(name: key, value: value)
+      }
     }
     
     return components
