a7639c50edb2f21716a1391d767b7807c37d6d56
Mark Smith <mark@camazotz.com>
2016-05-03 20:05:47 -0400

    Improve markdown stripping.

--- App/Repo.swift
+++ App/Repo.swift
@@ -4,12 +4,14 @@ class Repo {
   private static let title1Regex = try! NSRegularExpression(pattern: "^#+ +", options: .anchorsMatchLines)
   private static let title2Regex = try! NSRegularExpression(pattern: "^=+$", options: .anchorsMatchLines)
   private static let codeBlockRegex = try! NSRegularExpression(pattern: "^```.*$", options: .anchorsMatchLines)
-  
+
+  private static let imageRegex = try! NSRegularExpression(pattern: "!\\[(.*?)\\] ?\\(.*?\\)", options: .dotMatchesLineSeparators)
+  private static let linkRegex = try! NSRegularExpression(pattern: "\\[(.*?)\\] ?\\(.*?\\)", options: .dotMatchesLineSeparators)
+
   private static let anchorRegex = try! NSRegularExpression(pattern: "<a .*?>(.*?)</a>", options: .dotMatchesLineSeparators)
   private static let imgRegex = try! NSRegularExpression(pattern: "<img .*?/?>", options: .dotMatchesLineSeparators)
   private static let commentRegex = try! NSRegularExpression(pattern: "<!--.*?-->", options: .dotMatchesLineSeparators)
   
-  private static let linkRegex = try! NSRegularExpression(pattern: "!?\\[(.*?)\\] ?\\(.*?\\)", options: [])
   private static let bold1Regex = try! NSRegularExpression(pattern: "__(.+?)__", options: [])
   private static let bold2Regex = try! NSRegularExpression(pattern: "\\*\\*(.+?)\\*\\*", options: [])
   private static let italic1Regex = try! NSRegularExpression(pattern: "_(.+?)_", options: [])
@@ -65,12 +67,14 @@ class Repo {
     text = self.title1Regex.stringByReplacingMatches(in: text, options: [], range: NSRange(location: 0, length: text.characters.count), withTemplate: "")
     text = self.title2Regex.stringByReplacingMatches(in: text, options: [], range: NSRange(location: 0, length: text.characters.count), withTemplate: "")
     text = self.codeBlockRegex.stringByReplacingMatches(in: text, options: [], range: NSRange(location: 0, length: text.characters.count), withTemplate: "")
-    
+
+    text = self.imageRegex.stringByReplacingMatches(in: text, options: [], range: NSRange(location: 0, length: text.characters.count), withTemplate: "")
+    text = self.linkRegex.stringByReplacingMatches(in: text, options: [], range: NSRange(location: 0, length: text.characters.count), withTemplate: "$1")
+
     text = self.anchorRegex.stringByReplacingMatches(in: text, options: [], range: NSRange(location: 0, length: text.characters.count), withTemplate: "")
     text = self.imgRegex.stringByReplacingMatches(in: text, options: [], range: NSRange(location: 0, length: text.characters.count), withTemplate: "")
     text = self.commentRegex.stringByReplacingMatches(in: text, options: [], range: NSRange(location: 0, length: text.characters.count), withTemplate: "")
     
-    text = self.linkRegex.stringByReplacingMatches(in: text, options: [], range: NSRange(location: 0, length: text.characters.count), withTemplate: "$1")
     text = self.bold1Regex.stringByReplacingMatches(in: text, options: [], range: NSRange(location: 0, length: text.characters.count), withTemplate: "$1")
     text = self.bold2Regex.stringByReplacingMatches(in: text, options: [], range: NSRange(location: 0, length: text.characters.count), withTemplate: "$1")
     text = self.italic1Regex.stringByReplacingMatches(in: text, options: [], range: NSRange(location: 0, length: text.characters.count), withTemplate: "$1")
